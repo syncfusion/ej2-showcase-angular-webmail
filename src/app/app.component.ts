6af9ebcd50e8b642fddac2a8af24cafe1cc3e43d
ROOT\soundarapandianr <soundarapandianr@syncfusion.com>
2018-03-20 12:56:01 +0530

    Updated the CSS files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
     constructor() {
